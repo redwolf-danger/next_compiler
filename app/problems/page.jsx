04582cdb8ca2db42063dbcef1041983b52cd98f6
redwolf-danger <soniadvaita@gmail.com>
2024-07-01 19:46:33 +0530

    modiifcation for problems page

--- app/problems/page.jsx
+++ app/problems/page.jsx
@@ -4,7 +4,13 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div>
-      <Link href="./problems/1">sum problem</Link>
+      <ul>
+      {
+        Object.keys(question_list).map((value)=>{
+          return <li key={value}><Link href= {`./problems/${value}`}>question_list[value].title</Link> question_list[value].status</li>
+        })
+      }
+      </ul>
     </div>
   )
 }
